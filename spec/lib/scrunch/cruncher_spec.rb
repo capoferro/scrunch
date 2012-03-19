cd256e8bb9236d39f7ac3e175ec43ddd0c09ec99
Josiah Kiehl <jkiehl@riotgames.com>
2012-03-19 01:28:15 -0700

    process -> parse

--- spec/lib/scrunch/cruncher_spec.rb
+++ spec/lib/scrunch/cruncher_spec.rb
@@ -14,20 +14,20 @@ module Scrunch
         -> { c.crunch }.should_not raise_error
       end
     end
-    describe '#process' do
+    describe '#parse' do
       describe 'a damage line' do
         before do
           @damage_line = '[03/18/2012 03:59:24] [@Ahri] [Forge Guardian {795067165966336}] [Force Leap {812105301229568}] [ApplyEffect {836045448945477}: Damage {836045448945501}] (73 energy {836045448940874}) <73>'
         end
 
-        describe 'if the source does not exist prior to the line being processed' do
+        describe 'if the source does not exist prior to the line being parsed' do
           it 'should take a line and update the appropriate source with damage done' do
             subject.parse @damage_line
             subject.sources['@Ahri'].should == 73
           end
         end
         
-        describe 'if the source exists prior to the line being processed' do
+        describe 'if the source exists prior to the line being parsed' do
           it 'should increment damage done' do
             2.times { subject.parse @damage_line }
             subject.sources['@Ahri'].should == 146
